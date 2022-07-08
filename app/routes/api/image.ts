30b3ee592851f47baf94fe1bd6679ff8471f6566
Arif Ullah <arif.ullah@morrisonsplc.co.uk>
2022-07-08 15:32:26 +0100

    save

--- app/routes/api/image.ts
+++ app/routes/api/image.ts
@@ -1,9 +1,39 @@
+import os from "os";
+import path from "path";
 import type { LoaderFunction } from "@remix-run/node";
-import { imageLoader, DiskCache } from "remix-image/server";
+import type { Resolver } from "remix-image/server";
+import {
+  imageLoader,
+  DiskCache,
+  fsResolver,
+  fetchResolver,
+} from "remix-image/server";
+import { sharpTransformer } from "remix-image-sharp";
+
+export const fetchImage: Resolver = async (asset, url, options, basePath) => {
+  if (url.startsWith("/") && (url.length === 1 || url[1] !== "/")) {
+    return fsResolver(asset, url, options, basePath);
+  } else {
+    return fetchResolver(asset, url, options, basePath);
+  }
+};
+
+const vercelUrl = process.env.VERCEL_URL || "";
+const fixedVercelUrl = vercelUrl.startsWith("https")
+  ? vercelUrl
+  : `https://${vercelUrl}`;
 
 const config = {
-  selfUrl: "http://localhost:3000",
-  cache: new DiskCache(),
+  selfUrl:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : fixedVercelUrl,
+  cache: new DiskCache({
+    path: path.join(os.tmpdir(), "img"),
+  }),
+  resolver: fetchImage,
+  transformer: sharpTransformer,
+  basePath: process.env.NODE_ENV === "development" ? "public" : "/",
 };
 
 export const loader: LoaderFunction = ({ request }) => {
